ed2af9fb0bca56b2c154cb78fc51643f2faaee95
Laura TUFO <laura.tufo94@gmail.com>
2024-08-01 19:32:31 +0200

    Add login page + Nav and footer in App.jsx and add conditional background

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 function App() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login";
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
-    </Router>
+    <div className="app-container">
+      <Navigation />
+      <main className={`content ${isLoginPage ? "login-background" : ""}`}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,38 +1,32 @@
-import Navigation from "../composents/Navigation";
-import Hero from "../composents/Hero";
-import Feature from "../composents/Feature";
-import Footer from "../composents/Footer";
+import Hero from "../components/Hero";
+import Feature from "../components/Feature";
 import chatIcon from "../assets/img/icon-chat.png";
 import moneyIcon from "../assets/img/icon-money.png";
 import securityIcon from "../assets/img/icon-security.png";
 
 function HomePage() {
   return (
-    <div>
-      <Navigation />
-      <main>
-        <Hero />
-        <section className="features">
-          <h2 className="sr-only">Features</h2>
-          <Feature
-            icon={chatIcon}
-            title="You are our #1 priority"
-            description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
-          />
-          <Feature
-            icon={moneyIcon}
-            title="More savings means higher rates"
-            description="The more you save with us, the higher your interest rate will be!"
-          />
-          <Feature
-            icon={securityIcon}
-            title="Security you can trust"
-            description="We use top of the line encryption to make sure your data and money is always safe."
-          />
-        </section>
-      </main>
-      <Footer />
-    </div>
+    <main>
+      <Hero />
+      <section className="features">
+        <h2 className="sr-only">Features</h2>
+        <Feature
+          icon={chatIcon}
+          title="You are our #1 priority"
+          description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+        />
+        <Feature
+          icon={moneyIcon}
+          title="More savings means higher rates"
+          description="The more you save with us, the higher your interest rate will be!"
+        />
+        <Feature
+          icon={securityIcon}
+          title="Security you can trust"
+          description="We use top of the line encryption to make sure your data and money is always safe."
+        />
+      </section>
+    </main>
   );
 }
 
